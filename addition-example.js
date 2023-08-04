abb112b14decfcd739559f022aa7f44f43236d9a
emre kıyak <emrekiyak2003@gmail.com>
2023-08-04 15:05:54 +0300

    addition example

---
+++ addition-example.js
@@ -0,0 +1,9 @@
+function addition(a,b){
+    if(typeof a=="number" && typeof b=="number"){
+            console.log(a+b)
+        }
+        else{
+            console.log("hata")
+        }
+}
+addition(-9,8)
