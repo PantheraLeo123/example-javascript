0ec1725005932a51f1d71bb12d7589cba38f7b5d
emre kıyak <emrekiyak2003@gmail.com>
2023-08-04 15:57:32 +0300

    convert minutes into seconds

---
+++ convert-minutes-into-seconds.js
@@ -0,0 +1,14 @@
+function convert(minutes){
+
+    if (typeof minutes=="number" && minutes>=0) {
+        var seconds= minutes*60
+    console.log(seconds)
+        
+    } else { 
+        console.log("hata")
+        
+    }
+
+    
+
+}convert(50)
