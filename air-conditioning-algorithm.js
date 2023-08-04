158addd67c917e910864d65426038c86a3b8095e
emre kıyak <emrekiyak2003@gmail.com>
2023-08-04 17:24:04 +0300

    a/c

--- air-conditioning-algorithm.js
+++ air-conditioning-algorithm.js
@@ -12,5 +12,28 @@
 // ortam sıcaklığı = > 36 derece == klima çıktısı "Sıcaklık çok yüksek Klima yetersiz"
 
 function AirConditioning(temperature) {
-    
+    if (typeof temperature == "number") {
+        if (temperature <= 14) {
+            console.log("Sıcaklık çok düşük Klima yetersiz")
+        }
+        else if (temperature >= 15 && temperature < 25) {
+            console.log("Sıcaklık düşük Klima 30 derecede çalışacak")
+
+        }
+        else if (temperature == 25) {
+            console.log("Optimum Sıcaklık değeri-Klima Kapalı")
+        }
+        else if (temperature >= 26 && temperature <= 35) {
+            console.log("Sıcaklık yüksek Klima 24 derecede çalışacak")
+        }
+        else {
+            console.log("Sıcaklık çok yüksek Klima yetersiz")
+        }
+
+    } else {
+        console.log("Sıcaklık Değeri Hatalı")
+    }
 }
+
+
+AirConditioning(36)
