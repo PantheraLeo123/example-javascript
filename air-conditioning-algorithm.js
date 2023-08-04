028c0027af86baabb15bcb1357ab92d16b17eae1
emre kıyak <emrekiyak2003@gmail.com>
2023-08-04 16:33:54 +0300

    initial commit

---
+++ air-conditioning-algorithm.js
@@ -0,0 +1,16 @@
+// ------- Klima Algoritması -------
+// Ortamdaki sıcaklık değerine göre klimanın kaç derecede çalışacağı
+// belirlemek veya klimanın çalışmayacağını belirlemek için bir fonksiyon
+// üreteceğiz
+
+// --Test sıcaklıkları--
+
+// ortam sıcaklığı = < 14 derece == klima çıktısı "Sıcaklık çok düşük Klima yetersiz"
+// ortam sıcaklığı =15-25 derece == klima çıktısı "Sıcaklık düşük Klima 30 derecede çalışacak"
+// ortam sıcaklığı == 25  derece == klima çıktısı "Optimum Sıcaklık değeri-Klima Kapalı"
+// ortam sıcaklığı =26-35 derece == klima çıktısı "Sıcaklık yüksek Klima 24 derecede çalışacak"
+// ortam sıcaklığı = > 36 derece == klima çıktısı "Sıcaklık çok yüksek Klima yetersiz"
+
+function AirConditioning(temperature) {
+    
+}
